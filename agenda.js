bdc79c55227cfb9987519cdcb014699cf6638b84
josejuansorribes <josejuan.sorribes@gmail.com>
2015-05-17 23:01:10 +0200

    Entregable P2P módulo 2 terminado

--- agenda.js
+++ agenda.js
@@ -4,12 +4,18 @@ function agenda(titulo, inic){
 	return {
 		titulo: function() {return _titulo;},
 		meter: function(nombre,tf){_contenido[nombre]=tf;},
-		tf: function(nombre) {return_contenido[nombre];},
-		borrar: function(nombre){ delete_contenido[nombre];},
-		toJSON:function(){return JSON.stringify(_contenido);}
+		tf: function(nombre) {return _contenido[nombre];},
+		borrar: function(nombre){ delete _contenido[nombre];},
+		toJSON:function(){return JSON.stringify(_contenido);},
+		listar:function(){
+			var str='';
+			for(var i in _contenido){
+				str+= i + ', ' + _contenido[i] + '\n';
+			}
+			console.log(str);
+		}
 	}
 }
 
-var amigos= agenda("Amigos",{Pepe:113278561,José: 157845234, Jesús: 178512355});
-
- 
+var amigos= agenda("Amigos",{'Pepe':113278561, 'José': 157845234,'Jesús': 178512355});
+amigos.listar(); 
